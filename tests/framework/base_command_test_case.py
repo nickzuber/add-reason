96df0b3d4308aee5c71b9b3c7a2e3d14e2682307
Nicholas Zuber <zuber.nicholas@gmail.com>
2017-12-29 22:18:20 -0500

    Create more init tests

--- tests/framework/base_command_test_case.py
+++ tests/framework/base_command_test_case.py
@@ -6,26 +6,42 @@ from unittest import TestCase
 class BaseCommandTestCase(TestCase):
   
   _working_directory = os.path.dirname(os.path.realpath(__file__))
-  devnull = open(os.devnull, 'w')
+  _success = 'success'
+  _failed = 'failed'
+  _warning = 'warning'
 
   def call(self, *commands):
     response = subprocess.check_output(list(commands))
-    response = bytes(response)
     parse_output_line = r'\[(\d)/(\d)\] .*  ([a-zA-Z ]*)... (\w*)([a-zA-Z\'\"\!\?\(\)\[\]\:\; ]*)?'
-    list_of_parsed_lines = []
+    dict_of_parsed_lines = dict(steps=[], total_steps=0)
     for line in response.split("\n"):
       matches = re.match(parse_output_line, line)
       if (matches is not None):
         # 1: current step number
         # 2: total step numbers
         # 3: title of the step
-        # 4: result of the step
+        # 4: success of the step
         # 5: failure message
         line_as_list = []
         for i in range(1, matches.lastindex + 1):
           line_as_list.append(matches.group(i))
-        list_of_parsed_lines.append(line_as_list)
-    return list_of_parsed_lines
+
+        dict_of_parsed_lines['steps'].append((
+          line_as_list[2],  # title of step
+          line_as_list[3],  # success of step
+          line_as_list[4]   # failure message
+        ))
+        dict_of_parsed_lines['total_steps'] = int(line_as_list[1])
+    return dict_of_parsed_lines
+
+  def is_success(self, result):
+    return result == self._success
+
+  def is_failed(self, result):
+    return result == self._failed
+
+  def is_warning(self, result):
+    return result == self._warning
 
   def tearDown(self):
     pass
--- tests/test_init.py
+++ tests/test_init.py
@@ -10,22 +10,40 @@ class TestInit(BaseCommandTestCase):
   name = "reason-package"
   directory = "src/myCode"
   
-  def test_regular(self):
-    with cd("./tests/mockRoot"):
-      response = self.call("add-reason", "init", self.directory, self.name)
-      for line in response:
-        print(line)
-      self.assertEqual(1, 1)
+  def test_steps_pass(self):
+    with cd('./tests/mockRoot'):
+      results = self.call("add-reason", "init", self.directory, self.name)
+      all_steps_passed = True
+      for _, result, _ in results['steps']:
+        if not self.is_success(result):
+          all_steps_passed = False
+      self.assertTrue(all_steps_passed, 'All steps did not pass successfully')
+      self.assertEqual(results['total_steps'], 5, 'The total number of steps was not 5')
 
-  def test_no_linking(self):
-    with cd("./tests/mockRoot"):
-      response = self.call("add-reason", "init", self.directory, self.name, "--no-linking")
-      for line in response:
-        print(line)
-      self.assertEqual(1, 1)
+  def test_steps_pass_no_linking(self):
+    with cd('./tests/mockRoot'):
+      results = self.call("add-reason", "init", self.directory, self.name, "--no-linking")
+      all_steps_passed = True
+      for _, result, _ in results['steps']:
+        if not self.is_success(result):
+          all_steps_passed = False
+      self.assertTrue(all_steps_passed, 'All steps did not pass successfully')
+      self.assertEqual(results['total_steps'], 3, 'The total number of steps was not 3')
+
+  def test_bsconfig_was_created(self):
+    with cd('./tests/mockRoot'):
+      results = self.call("add-reason", "init", self.directory, self.name)
+      exists = os.path.isfile('bsconfig.json')
+      self.assertTrue(exists, 'bsconfig.json was never created')
+
+  def test_merlin_was_created(self):
+    with cd('./tests/mockRoot'):
+      results = self.call("add-reason", "init", self.directory, self.name)
+      exists = os.path.isfile('.merlin')
+      self.assertTrue(exists, '.merlin was never created')
 
   def tearDown(self):
-    with cd("./tests/mockRoot"):
+    with cd('./tests/mockRoot'):
       self.call("rm", "-f", "bsconfig.json")
       self.call("rm", "-f", ".merlin")
       self.call("rm", "-rf", "lib")
